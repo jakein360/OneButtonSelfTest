baf4300551841805e8a6d4e45594c5646536ddd0
Jake Emitte <jakeemitte829@gmail.com>
2025-06-26 21:37:45 -0700

    Initial Commit

---
+++ OneButtonSelfTest/OneButtonSelfTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneButtonSelfTestApp.swift
+//  OneButtonSelfTest
+//
+//  Created by Jake Emitte on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OneButtonSelfTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
