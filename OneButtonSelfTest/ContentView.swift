4399378b3916f285bc6f9980027dd3cd7c01768d
Jake Emitte <jakeemitte829@gmail.com>
2025-06-26 22:12:12 -0700

    Challenged myself to rewrite this app from scratch using arrays for the messages. app has an app icon, launch screen and functions the same as the you are awesome project app up to chapter 1.9.

--- OneButtonSelfTest/ContentView.swift
+++ OneButtonSelfTest/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image = ""
+    @State private var imageNumber = 0
+    @State private var message = ""
+    @State private var messageNumber = 0
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(image)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+                .shadow(radius: 25)
+            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .foregroundColor(.mint)
+            
+            Spacer()
+            Button("Press Me!") {
+                
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                image = "image\(imageNumber)"
+                
+                let messages = ["You are Awesome!",
+                                "You Are Great!",
+                                "You are Dumb!",
+                                "You Hate Arrays!"]
+                
+                messageNumber += 1
+                if messageNumber > 3 {
+                    messageNumber = 0
+                }
+                
+                message = messages[messageNumber]
+                
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.largeTitle)
+            .tint(.orange)
+            
+            
+            
+            
         }
         .padding()
     }
